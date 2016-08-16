ec0c92fa929fa1bf298899eca5ae8fbda8cd6cfa
Aaron Crickenberger <spiffxp@gmail.com>
2016-08-16 10:19:41 -0700

    Default to kubernetes v1.3.5

--- terraform/local/variables.tf
+++ terraform/local/variables.tf
@@ -113,7 +113,7 @@ variable "deployment_mode" {
 }
 
 variable "hyperkube_image" {
-  default     = "gcr.io/google_containers/hyperkube:v1.3.3"
+  default     = "gcr.io/google_containers/hyperkube:v1.3.5"
   description = "image to use when running with hyperkube_deploy_mode 'docker'"
 }
 
